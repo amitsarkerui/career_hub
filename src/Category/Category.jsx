eef9dd0a80c509ae0919c27c0b4f2a1338f65ef6
amitsarkerui <amitsarkerstock@gmail.com>
2023-04-10 10:46:24 +0600

    category section done

--- src/Category/Category.jsx
+++ src/Category/Category.jsx
@@ -12,13 +12,22 @@ const Category = () => {
       .then((data) => setCategory(data));
   }, []);
   return (
-    <div>
-      {category.map((singleCategory) => (
-        <CategorySingle
-          key={singleCategory.id}
-          singleCategory={singleCategory}
-        ></CategorySingle>
-      ))}
+    <div className="container mx-auto px-4 my-24">
+      <div className="text-center mb-10">
+        <h1 className="text-5xl font-bold mb-5">Job Category List</h1>
+        <p>
+          Explore thousands of job opportunities with all the information you
+          need. Its your future
+        </p>
+      </div>
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-4">
+        {category.map((singleCategory) => (
+          <CategorySingle
+            key={singleCategory.id}
+            singleCategory={singleCategory}
+          ></CategorySingle>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Category/CategorySingle/CategorySingle.jsx
+++ src/Category/CategorySingle/CategorySingle.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const CategorySingle = ({ singleCategory }) => {
   return (
-    <div>
-      <h1>{singleCategory.categoryName}</h1>
+    <div className="bg-purple-50 rounded-lg p-8">
+      <img
+        className="bg-purple-100 p-3 rounded-md"
+        src={singleCategory.categoryIcon}
+        alt=""
+      />
+      <h2 className="text-xl font-bold mt-8 mb-3">
+        {singleCategory.categoryName}
+      </h2>
+      <p>{singleCategory.availableJobs}</p>
     </div>
   );
 };
